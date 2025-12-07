6d37ae17f855c1e9297ca30c25d0d4af84e17bc3
thenikolayli <nikolay.li2008@gmail.com>
2025-12-07 02:50:28 -0800

    paraglide + reworked header

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -6,20 +6,41 @@
     import { Draggable } from "gsap/Draggable";
     import { InertiaPlugin } from "gsap/InertiaPlugin";
     import Carousel from "$lib/components/carousel.svelte";
+    import { setLocale } from "$lib/paraglide/runtime";
+    import { m } from "$lib/paraglide/messages.js";
+
+    let openMenu = $state(false);
+    let header;
+
+    $effect(() => {
+        if (openMenu) {
+            gsap.to(header, {
+                height: innerHeight * 0.35,
+                duration: 0.5,
+                ease: "power2.inOut",
+            });
+        } else {
+            gsap.to(header, {
+                height: innerHeight * 0.06,
+                duration: 0.5,
+                ease: "power2.inOut",
+            });
+        }
+    });
 
     let landingPhoto;
     let miniPhotoSection;
     const cards = [
         {
-            date: "June, 2025",
-            title: "Some Game",
-            description: "small paragraph",
+            title: m["cards.card1.title"](),
+            date: m["cards.card1.date"](),
+            description: m["cards.card1.description"](),
             src: "/photos/inCar.JPEG",
         },
         {
-            date: "June, 2025",
-            title: "Some Game",
-            description: "small paragraph",
+            title: m["cards.card1.title"](),
+            date: m["cards.card1.date"](),
+            description: m["cards.card1.description"](),
             src: "/photos/onField1.png",
         },
     ];
@@ -29,6 +50,8 @@
     gsap.registerPlugin(InertiaPlugin);
     onMount(() => {
         document.title = "Andrey Li";
+        addEventListener("click", handleClickOutside);
+        return () => removeEventListener("click", handleClickOutside);
 
         gsap.set(landingPhoto, {
             "--t": 0,
@@ -58,32 +81,61 @@
             ease: "none",
         });
     });
+
+    const handleClickOutside = (event) => {
+        if (!header.contains(event.target)) {
+            openMenu = false;
+        }
+    };
 </script>
 
 <header
-    class="fixed h-[6vh] bg-stone-100/40 backdrop-blur-sm p-8 text-3xl z-30 w-full shadow-lg flex items-center justify-between"
+    bind:this={header}
+    class="fixed h-[6vh] bg-stone-100/40 backdrop-blur-sm px-8 text-3xl z-30 w-full shadow-lg flex flex-col overflow-hidden"
 >
-    <div>
-        <span class="text-sblue">Andrey&nbsp;</span><span class="text-sred"
-            >Li</span
+    <section class="flex items-center justify-between h-[6vh] shrink-0">
+        <div>
+            <span class="text-sblue mr-2">{m.first_name()}</span>
+            <span class="text-sred">{m.last_name()}</span>
+        </div>
+        <button
+            aria-label="Open language menu"
+            onclick={() => (openMenu = !openMenu)}
         >
-    </div>
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        ><g
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="1.2"
-            ><path
-                d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0-18 0m.6-3h16.8M3.6 15h16.8"
-            /><path d="M11.5 3a17 17 0 0 0 0 18m1-18a17 17 0 0 1 0 18" /></g
-        ></svg
-    >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+                ><g
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.2"
+                    ><path
+                        d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0-18 0m.6-3h16.8M3.6 15h16.8"
+                    /><path
+                        d="M11.5 3a17 17 0 0 0 0 18m1-18a17 17 0 0 1 0 18"
+                    /></g
+                ></svg
+            >
+        </button>
+    </section>
+    <section class="w-full pt-4">
+        <h1 class="text-sblue">Languages</h1>
+        <ul class="text-2xl mt-2">
+            <li>
+                <button onclick={() => setLocale("en")}> English </button>
+            </li>
+            <li>
+                <button onclick={() => setLocale("es")}> Spanish </button>
+            </li>
+            <li>
+                <button onclick={() => setLocale("ru")}> Russian </button>
+            </li>
+        </ul>
+    </section>
 </header>
 
 <section class="relative w-full h-screen bg-stone-100">
@@ -103,9 +155,9 @@
     <div
         class="absolute w-full h-[25vh] py-4 px-8 bottom-0 left-0 flex flex-col"
     >
-        <h1 class="text-4xl text-sblue font-semibold">Andrey Li</h1>
+        <h1 class="text-4xl text-sblue font-semibold">{m.full_name()}</h1>
         <p class="text-stone-600 text-2xl mt-2">
-            Introduction about the athlete, about a paragraph long.
+            {m.introduction()}
         </p>
     </div>
 </section>
@@ -114,7 +166,7 @@
     class="relative w-full h-screen bg-stone-100 pt-[6vh] px-0 flex flex-col items-center justify-center"
 >
     <header class="w-full font-semibold text-sblue text-4xl px-8 py-4">
-        Highlights
+        {m.highlights()}
     </header>
     <div
         class="relative w-full flex-1 flex items-center justify-start overflow-hidden pb-8 px-8 space-x-8"
@@ -128,7 +180,7 @@
 >
     <img class="object-contain" src="/photos/withTrophy2.png" alt="" />
     <h1 class="text-3xl text-sblue text-center font-semibold mt-2">
-        Follow my Instagram!
+        {m.instagram()}
     </h1>
     <a
         target="_blank"
@@ -151,4 +203,8 @@
             class="absolute w-full h-full object-cover left-0 top-0"
         />
     </a>
+
+    <div class="absolute p-1 bottom-0 text-sm text-sblue">
+        a Nikolay Li production
+    </div>
 </section>
